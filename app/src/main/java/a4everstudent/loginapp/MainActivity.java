cbfb6e22ce8a26991902359d8e6207e02c0f4129
4EverStudent <mail@4everstudent.com>
2016-08-29 10:34:33 +0100

    Add Username and Password Add Login Button Add Function to show username and pass on log

--- app/src/main/java/a4everstudent/loginapp/MainActivity.java
+++ app/src/main/java/a4everstudent/loginapp/MainActivity.java
@@ -1,9 +1,20 @@
 package a4everstudent.loginapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void login(View view){
+        EditText user = (EditText) findViewById(R.id.user);
+        EditText pass = (EditText) findViewById(R.id.pass);
+        Log.i("User", user.getText().toString());
+        Log.i("Pass", pass.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
