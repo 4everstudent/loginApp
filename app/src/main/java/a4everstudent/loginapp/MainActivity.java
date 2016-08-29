74903609a3e42a73216720e0614b127d112de8e2
4EverStudent <mail@4everstudent.com>
2016-08-29 11:13:56 +0100

    Add toast that says Hello using username.

--- app/src/main/java/a4everstudent/loginapp/MainActivity.java
+++ app/src/main/java/a4everstudent/loginapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +13,15 @@ public class MainActivity extends AppCompatActivity {
     public void login(View view){
         EditText user = (EditText) findViewById(R.id.user);
         EditText pass = (EditText) findViewById(R.id.pass);
+
+        //shows username and password on log
         Log.i("User", user.getText().toString());
         Log.i("Pass", pass.getText().toString());
+
+        //Says hello to the user using a toast
+        CharSequence text = "Hello "+user.getText()+"!";
+        Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
